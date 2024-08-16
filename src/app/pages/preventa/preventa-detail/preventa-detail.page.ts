bd69e23b2ccdf65ca27b3ae22b201de83f9a634b
ATellez22 <adolfo.tellez1991@gmail.com>
2024-08-16 16:54:05 -0400

    added-details-in-whatsapp-message

--- src/app/pages/preventa/preventa-detail/preventa-detail.page.ts
+++ src/app/pages/preventa/preventa-detail/preventa-detail.page.ts
@@ -21,18 +21,35 @@ export class PreventaDetailPage implements OnInit {
     this.setPreventaDetail();
   }
 
-  async setPreventaDetail(){
-    this.preventa = await this.preventaService.getPreventaDetail(this.preventaId);
+  async setPreventaDetail() {
+    this.preventa = await this.preventaService.getPreventaDetail(
+      this.preventaId
+    );
     console.log(this.preventa);
   }
 
   openWhatsApp(preventaId: string) {
-    const message = `
-      Tx: ${preventaId}
-      Fecha: ${this.preventa.fecha}
-      Total: ${this.preventa.total}
-      Estado: ${this.preventa.estado}
-    `;
+    let message = `Tx: ${preventaId}\n`;
+    message += `Fecha: ${this.preventa.fecha}\n`;
+
+    message += `Nombre: ${this.preventa.cliente.nombre}\n`;
+    message += `RUC/CI: ${
+      this.preventa.cliente.ruc || this.preventa.cliente.ci
+    }\n`;
+
+    message += `Total: ${this.preventa.total}\n`;
+    message += `Estado: ${this.preventa.estado}\n\n`;
+
+    message += `Detalle de productos:\n`;
+
+    this.preventa.detalle.forEach((producto: any, index: number) => {
+      message += `${index + 1}. ${producto.producto.nombre}\n`;
+      message += `   Cantidad: ${producto.cantidad}\n`;
+      message += `   Presentación: ${producto.tipo_envase}\n`;
+      message += `   Precio Unitario: ${producto.precio_unitario}\n`;
+      message += `   Subtotal: ${producto.subtotal}\n\n`;
+    });
+
     const encodedMessage = encodeURIComponent(message);
     const url = `https://wa.me/?text=${encodedMessage}`;
     window.open(url, '_blank');
