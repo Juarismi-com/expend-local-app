7fdb8c1e99d3b5c8b4176ad66fa1520eeec6f919
Ivan Fretes <vanfretes@gmail.com>
2025-07-03 14:25:41 -0300

    fix: mainactivity

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -1,18 +1,29 @@
 package io.ionic.starter;
 
 import com.getcapacitor.BridgeActivity;
+import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends BridgeActivity {
-   @Override
-   protected void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-      
-      // Activa el modo inmersivo
-      getWindow().getDecorView().setSystemUiVisibility(
-         View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-         | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-         | View.SYSTEM_UI_FLAG_FULLSCREEN
-      );
-   }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            enableImmersiveMode();
+        }
+    }
+
+    private void enableImmersiveMode() {
+        getWindow().getDecorView().setSystemUiVisibility(
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            | View.SYSTEM_UI_FLAG_FULLSCREEN
+        );
+    }
 }
